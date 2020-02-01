5d6cb71429947d060fb5a209d8cbcf386cbabf4f
Nico Cobb <cobbn@rpi.edu>
2020-02-01 16:20:17 -0500

    I steal my own code again

---
+++ Assets/Script/Checkpoint.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : RaycastController {
+
+	public RespawnManager respawnManager;
+    public LayerMask checkMask;
+    public float checkCircleSize = 2f;
+
+    // Use this for initialization
+    public override void Start () {
+		respawnManager = FindObjectOfType<RespawnManager> ();
+	}
+	
+
+    void Update()
+    {
+        UpdateRaycastOrigins();
+        CheckWinCondition();
+    }
+
+    void CheckWinCondition()
+    {
+        Vector2 rayOrigin = raycastOrigins.center;
+        Collider2D collider = Physics2D.OverlapCircle(rayOrigin, checkCircleSize, checkMask);
+
+        if (collider)
+        {
+            respawnManager.currentCheckpoint = gameObject;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector2 rayOrigin = raycastOrigins.center;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(rayOrigin, checkCircleSize);
+    }
+}
---
+++ Assets/Script/RespawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnManager : MonoBehaviour {
+
+	public GameObject currentCheckpoint;
+	private Player player;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.transform.position.y < -50f) {
+			respawnPlayer ();
+		}
+	}
+
+	public void respawnPlayer() {
+		player.transform.position = currentCheckpoint.transform.position;
+	}
+}
