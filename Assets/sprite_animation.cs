1eb626c2df1e7813054798f782fa5e53747e955c
estalcil134 <wilsonwong1688@gmail.com>
2020-02-02 13:25:02 -0500

    animation attempts were made

--- Assets/sprite_animation.cs
+++ Assets/sprite_animation.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class sprite_animation : MonoBehaviour
 {
+    // i dont understand sprite animation :(
+
+    public float walkSpeed = 1;
+    private bool _isGrounded = false;
+    const int IDLE = 0;
+    const int WALK = 1;
+    const int JUMP = 2;
+    const int LAND = 3;
+
+    Animator animator;
+
+    bool _isWalk = false;
+
+    string currDir = "right";
+    int _currAniState = IDLE;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        if(Input.GetKey("space") && _isGrounded)
+        {
+            changeState(JUMP);
+            _isGrounded = false;
+        }
+        else if (!_isGrounded && )
+        {
+
+        }
+        */
     }
 }
