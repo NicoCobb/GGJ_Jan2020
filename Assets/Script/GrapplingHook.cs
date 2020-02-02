c225d598488c1fda50fc5f4937d0b24aba16ef8f
Nico Cobb <cobbn@rpi.edu>
2020-02-02 10:21:51 -0500

    grapply stuff

--- Assets/Script/GrapplingHook.cs
+++ Assets/Script/GrapplingHook.cs
@@ -7,7 +7,7 @@ public class GrapplingHook : MonoBehaviour
     //palyer reference
     Player p;
     float grappleShotVelocity;
-    Vector2 mouseClickDir;
+    Vector2 mouseClickLoc;
 
     LineRenderer lr;
 
@@ -20,7 +20,7 @@ public class GrapplingHook : MonoBehaviour
 
     public void AimTongue(Vector2 mouseClick, Player pl, float shootVel) {
 
-        mouseClickDir = mouseClick;
+        mouseClickLoc = mouseClick;
         p = pl;
         grappleShotVelocity = shootVel;
 
@@ -31,15 +31,19 @@ public class GrapplingHook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 setPosOne = transform.position;
+        if ((Vector2)transform.position == mouseClickLoc) {
+ 
+        }
         
         //also collision stuff
-        transform.position += (Vector3)mouseClickDir*grappleShotVelocity;
+        transform.position += (Vector3)mouseClickLoc*grappleShotVelocity;
 
-       lr.positionCount = 2;
-       //set start point to player transform origin
-       //TODO: Set grappleOrigin and use instead
-       lr.SetPosition(0,p.transform.position);
-       //set endpoint to start + toClickLocation * grappleShotSpeed? 
-       lr.SetPosition(1, transform.position);
+        lr.positionCount = 2;
+        //set start point to player transform origin
+        //TODO: Set grappleOrigin and use instead
+        lr.SetPosition(0,p.transform.position);
+        //set endpoint to start + toClickLocation * grappleShotSpeed? 
+        lr.SetPosition(1, transform.position);
     }
 }
